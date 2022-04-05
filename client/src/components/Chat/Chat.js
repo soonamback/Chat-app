39787447cc22b4b18e8f8b93d9b6138870c76844
soonamback <soonam.yi123@gmail.com>
2022-04-05 22:55:16 +0900

    location search

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,27 +1,32 @@
 import React, { useState, useEffect } from "react";
 import queryString from 'query-string';
+//const queryString = require('query-string');
 import io from 'socket.io-client'
+
+import './Chat.css';
 
 let socket;
 
-const Chat = ({location}) =>
+const Chat = ({ location }) =>
 {
+    //const parsed = queryString.parse(location.search)
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');
+    
     const ENDPOINT = 'localhost:5000';
     
-    useEffect(() =>
-    {
-        const { name, room } = queryString.parse(location.search);
+    const parsed = queryString.parse(location.search);
 
+
+
+    useEffect(() => {
+        
+        const { name, room } = queryString.parse(parsed.location.search);
         socket = io(ENDPOINT);
-
-        setName(name);
+    
         setRoom(room);
-
-        console.log(socket);
-        socket.emit('join', { name, room });
-    }, [ENDPOINT, location.search]);
+        setName(name);
+    }, [ENDPOINT, location.search]);   
     return (
         <h2>chat</h2>
     )
