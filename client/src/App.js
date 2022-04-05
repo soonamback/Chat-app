8e18b3e5e46a299a4f3f0808c6481707d642ee17
soonamback <soonam.yi123@gmail.com>
2022-04-05 21:19:16 +0900

    add chat component

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import './App.css';
+
+
+import Join  from './components/Join/Join'
+import Chat  from './components/Chat/Chat'
+
+function App() {
+  return (
+    <div className="App">
+    
+    <Router>
+      <Routes>
+      
+        <Route path="/" exact element={<Join />} />
+        
+        <Route path="/chat" exact element={<Chat />} />
+        
+        </Routes> 
+    </Router>
+    </div>
+  );
+}
+
+export default App;
